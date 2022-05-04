da508ac5bf16f1cc5015a4e335a8e9529215f2c2
Florent Bouisset <florent.bouisset@jellysmack.com>
2022-05-04 15:55:43 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
     <div id="nav"></div>
+    <h1>TEST {{env}}</h1>
     <router-view name="default" />
     <router-view name="modal" />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      env: process.env.VUE_APP_HELLO
+    }
+  }
+}
+</script>
 <style>
 #nav {
   padding: 30px;
